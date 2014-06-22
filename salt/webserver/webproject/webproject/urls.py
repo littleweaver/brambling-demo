2fb0df8a5dc9ed909666465e058965482289e5a4
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-06-21 21:41:19 -0400

    Added admin and feedback URLs to project.

--- salt/webserver/webproject/webproject/urls.py
+++ salt/webserver/webproject/webproject/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^feedback/', include('zenaida.contrib.feedback.urls')),
     url(r'^', include('brambling.urls')),
 )
