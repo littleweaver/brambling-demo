bb0bb0ffc62eb321828203c8aeae5a3c08acb507
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-11-04 12:25:26 -0800

    Added hints urls to webproject URLs.

--- salt/webserver/webproject/webproject/urls.py
+++ salt/webserver/webproject/webproject/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^feedback/', include('zenaida.contrib.feedback.urls')),
+    url(r'^hints/', include('zenaida.contrib.hints.urls')),
     url(r'^', include('brambling.urls')),
 )
