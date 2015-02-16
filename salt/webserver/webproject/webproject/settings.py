f3b12b64c2e667c4ab07a102074ca8065c3b89ec
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-02-16 00:51:14 -0800

    Enabled Grappelli admin.

--- salt/webserver/webproject/webproject/settings.py
+++ salt/webserver/webproject/webproject/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = (
     'brambling',
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- salt/webserver/webproject/webproject/urls.py
+++ salt/webserver/webproject/webproject/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^talkback/', include('talkback.urls')),
     url(r'^hints/', include('zenaida.contrib.hints.urls')),
